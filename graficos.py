b14941532eba71dc6d137e1e2531fc664806af8e
Thekiborg <doflr415@gmail.com>
2025-10-11 18:52:12 +0200

    Grafio incidendias pie

--- graficos.py
+++ graficos.py
@@ -1,34 +1,21 @@
 import matplotlib.pyplot as plt
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-import numpy as n
-import ArrayUtils as au
 
-# set_title es un titulo encima del grafico
-# X y Y son titulos en horizontal y diagonal respect
-
-def GraficoLineas(sizeX, sizeY, meses, valoresY, titulo = None, tituloX = None, tituloY = None):
+def GraficoTipoDeIncidencias(tipoIncidenciaYCuenta: list[tuple[str, int, str]], titulo):
     """ valoresX y valoresY son Arrays de numpy
     """
-    fig, ax = plt.subplots(figsize=(sizeX, sizeY))
-    bars = ax.bar(meses, valoresY, color="#060061")
 
-    for bar in bars:
-        height = bar.get_height()
-        ax.text(
-            bar.get_x() + bar.get_width() / 2,
-            height - 0.1,
-            str(height),
-            ha='center', va='top',
-            color='white', fontsize=10
-    )
+    labels = ["Bajo", "Medio", "Alto"]
+    sizes = [33, 12, 4]
+    colors = ["#75DAFF", "#FFFF88", "#FF8A96"]
+    # Pastel azul, amarillo y rojo
 
-    ax.set_title(titulo)
-    ax.set_xlabel(tituloX)
-    ax.set_ylabel(tituloY)
-    ax.set_yticks([])
-    ax.set_yticklabels([])
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors)
+    plt.title(titulo)
+    plt.axis("equal")
     plt.show()
     pass
 
-GraficoLineas(10, 40, ["enero", "febrero","marzo","abril"], [1, 3, 1, 1])
+GraficoTipoDeIncidencias(7, "Grafico de incidencias por tipo")
+
+# plt.savefig('foo.png')
+# plt.savefig('foo.pdf')
